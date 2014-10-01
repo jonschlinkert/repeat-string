0c2a3a0e75d18266b75e907715c55de61a1f80ba
jonschlinkert <dev@sellside.com>
2014-10-01 19:28:31 -0400

    v0.2.0
    refactor, simplify

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-module.exports = function(str, count) {
-  if (count < 1) {
-    return '';
+/*!
+ * repeat-string <https://github.com/jonschlinkert/repeat-string>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+module.exports = function repeat(str, num) {
+  if (typeof str !== 'string') {
+    throw new TypeError('repeat-string expects a string.');
   }
 
-  var result = '';
-  while (count > 0) {
-    if (count & 1) {
-      result += str;
-    }
-    count >>= 1;
-    str += str;
+  var res = '';
+
+  while (num--) {
+    res += str;
   }
-  return result;
-}
+
+  return res;
+};
