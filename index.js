4b54849e2637e7f33c64d0cd1d9e46717a6b8999
jonschlinkert <dev@sellside.com>
2014-10-14 13:39:27 -0400

    revert

--- index.js
+++ index.js
@@ -7,16 +7,22 @@
 
 'use strict';
 
-module.exports = function repeat(str, num) {
+module.exports = function repeat(str, count) {
   if (typeof str !== 'string') {
     throw new TypeError('repeat-string expects a string.');
   }
 
-  var res = '';
-
-  while (num--) {
-    res += str;
+  if (count < 1) {
+    return '';
   }
 
-  return res;
+  var result = '';
+  while (count > 0) {
+    if (count & 1) {
+      result += str;
+    }
+    count >>= 1;
+    str += str;
+  }
+  return result;
 };
