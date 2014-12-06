0be55513dcdf50464be4a6affd3736f8eda9bc49
jonschlinkert <dev@sellside.com>
2014-12-06 18:12:42 -0500

    faster implmentation

--- index.js
+++ index.js
@@ -7,20 +7,66 @@
 
 'use strict';
 
-module.exports = function repeat(str, num) {
+/**
+ * Expose `repeat`
+ */
+
+module.exports = repeat;
+
+/**
+ * Results cache
+ */
+
+var res = '';
+var cache;
+
+/**
+ * Repeat the given `string` the specified `number`
+ * of times.
+ *
+ * **Example:**
+ *
+ * ```js
+ * var repeat = require('repeat-string');
+ * repeat('A', 5);
+ * //=> AAAAA
+ * ```
+ *
+ * @param {String} `string` The string to repeat
+ * @param {Number} `number` The number of times to repeat the string
+ * @return {String} Repeated string
+ * @api public
+ */
+
+function repeat(str, num) {
   if (typeof str !== 'string') {
     throw new TypeError('repeat-string expects a string.');
   }
 
-  var res = '';
+  cache = cache || str;
+  if (cache !== str) {
+    res = '';
+  }
 
-  while (num) {
+  var max = (str.length * num);
+  var i = 0;
+
+  if (res.length >= max) {
+    return res.slice(0, max);
+  }
+
+  for (; num > 0; i++) {
     if (num & 1) {
       res += str;
     }
+
+    if (res.length >= max) {
+      return res.slice(0, max);
+    }
+
     num >>= 1;
     str += str;
   }
 
-  return res;
-};
+  return res.slice(0, max);
+}
