6220c373a3d075d6127823fb52738578a93a4af6
Linus Unnebäck <linus@folkdatorn.se>
2016-02-13 21:11:55 +0100

    benchmark: better output

--- benchmark/check.js
+++ benchmark/check.js
@@ -8,17 +8,30 @@ var glob = require('glob');
  * Sanity check. Run to ensure that all fns return the same result.
  */
 
-var fixtures = glob.sync(__dirname + '/fixtures/*.js');
+var fixtures = glob.sync(__dirname + '/fixtures/*.js').map(require);
+var expected = [0, 3, 300, 6, 6000, 60000, 75, 750, 9, 12, 15, 150];
 
 glob.sync(__dirname + '/code/*.js').forEach(function (fp) {
   var fn = require(path.resolve(__dirname, 'code', fp));
   var name = path.basename(fp, path.extname(fp));
-  if (/\.js/.test(fp) && /^rep.*/.test(name)) {
+  var problems = [];
 
-    fixtures.forEach(function (fixture) {
-      if (/^.*\.js/.test(path.basename(fixture))) {
-        console.log(chalk.bold(name) + ':', fn.apply(fn, require(fixture)).length);
-      }
+  fixtures.forEach(function (fixture, idx) {
+    var answer = fn.apply(fn, fixture).length;
+
+    if (answer !== expected[idx]) {
+      problems.push(['repeat(' + fixture.join(', ') + ').length', answer, expected[idx]]);
+    }
+  });
+
+  if (problems.length === 0) {
+    console.log(' ' + chalk.bold.green('✔') + ' ' + chalk.bold(name));
+  } else {
+    console.log(' ' + chalk.bold.red('✖') + ' ' + chalk.bold(name));
+
+    problems.forEach(function (item, idx, arr) {
+      var str = item[0] + ' gave ' + item[1] + ', expected ' + item[2];
+      console.log((idx === arr.length - 1 ? ' ┗ ' : ' ┣ ') + chalk.red(str));
     });
   }
 });
