31c0d5323c3c58f3a4ac576236018306e0ad640a
jonschlinkert <jon.schlinkert@sellside.com>
2016-10-23 12:45:43 -0400

    use assert

--- test.js
+++ test.js
@@ -8,55 +8,55 @@
 'use strict';
 
 require('mocha');
-require('should');
+var assert = require('assert');
 var repeat = require('./');
 
 describe('repeat', function() {
   it('should return an empty string when a number is not given:', function() {
-    repeat('a').should.equal('');
+    assert.equal(repeat('a'), '');
   });
 
   it('should return an empty string when zero or null is given as the number:', function() {
-    repeat('', 0).should.equal('');
-    repeat('a', 0).should.equal('');
-    repeat('', null).should.equal('');
-    repeat('a', null).should.equal('');
+    assert.equal(repeat('', 0), '');
+    assert.equal(repeat('a', 0), '');
+    assert.equal(repeat('', null), '');
+    assert.equal(repeat('a', null), '');
   });
 
   it('should repeat the given string n times', function() {
-    repeat(' ', 0).should.equal('');
-    repeat('a', 0).should.equal('');
-    repeat('a', 1).should.equal('a');
-    repeat('a', 2).should.equal('aa');
-    repeat('a', 3).should.equal('aaa');
-    repeat('   ', 3).should.equal('         ');
-    repeat('a ', 3).should.equal('a a a ');
-    repeat('a', 10).should.equal('aaaaaaaaaa');
-    repeat('b ', 10).should.equal('b b b b b b b b b b ');
-    repeat('a ', 10).should.equal('a a a a a a a a a a ');
-    repeat('abc ', 25).should.equal('abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc ');
+    assert.equal(repeat(' ', 0), '');
+    assert.equal(repeat('a', 0), '');
+    assert.equal(repeat('a', 1), 'a');
+    assert.equal(repeat('a', 2), 'aa');
+    assert.equal(repeat('a', 3), 'aaa');
+    assert.equal(repeat('   ', 3), '         ');
+    assert.equal(repeat('a ', 3), 'a a a ');
+    assert.equal(repeat('a', 10), 'aaaaaaaaaa');
+    assert.equal(repeat('b ', 10), 'b b b b b b b b b b ');
+    assert.equal(repeat('a ', 10), 'a a a a a a a a a a ');
+    assert.equal(repeat('abc ', 25), 'abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc ');
   });
 
   it('should allow the multiplier to be a string:', function() {
-    repeat('a', '0').should.equal('');
-    repeat('a', '1').should.equal('a');
-    repeat('a', '2').should.equal('aa');
-    repeat('a', '10').should.equal('aaaaaaaaaa');
-    repeat('b ', '10').should.equal('b b b b b b b b b b ');
-    repeat('a ', '10').should.equal('a a a a a a a a a a ');
+    assert.equal(repeat('a', '0'), '');
+    assert.equal(repeat('a', '1'), 'a');
+    assert.equal(repeat('a', '2'), 'aa');
+    assert.equal(repeat('a', '10'), 'aaaaaaaaaa');
+    assert.equal(repeat('b ', '10'), 'b b b b b b b b b b ');
+    assert.equal(repeat('a ', '10'), 'a a a a a a a a a a ');
   });
 
   it('should cache strings until the string changes:', function() {
-    repeat('a', '5').should.equal('aaaaa');
-    repeat('b ', '5').should.equal('b b b b b ');
-    repeat('a ', '5').should.equal('a a a a a ');
-    repeat('c ', '5').should.equal('c c c c c ');
-    repeat('a ', '5').should.equal('a a a a a ');
-    repeat('b ', '5').should.equal('b b b b b ');
+    assert.equal(repeat('a', '5'), 'aaaaa');
+    assert.equal(repeat('b ', '5'), 'b b b b b ');
+    assert.equal(repeat('a ', '5'), 'a a a a a ');
+    assert.equal(repeat('c ', '5'), 'c c c c c ');
+    assert.equal(repeat('a ', '5'), 'a a a a a ');
+    assert.equal(repeat('b ', '5'), 'b b b b b ');
   });
 
   it('should throw an error when no string is given:', function() {
-    (function() {repeat(10); }).should.throw('repeat-string expects a string.');
-    (function() {repeat(null); }).should.throw('repeat-string expects a string.');
+    assert.throws(function() {repeat(10); }, /expected a string/);
+    assert.throws(function() {repeat(null); }, /expected a string/);
   });
 });
